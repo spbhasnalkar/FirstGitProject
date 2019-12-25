2d0771c49a15daee965ef7ed313ad516420268e5
Hasnalkar Pralhad <Hasnalkar Pralhad@LAPTOP-7L44I5QS>
2019-12-25 23:57:10 +0530

    FirstCommit....

---
+++ src/com/FirstGitProject/FirstGitProject.java
@@ -0,0 +1,14 @@
+package com.FirstGitProject;
+
+public class FirstGitProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Related to Git and Github");
+
+	}
+
+}
+
+
+	
